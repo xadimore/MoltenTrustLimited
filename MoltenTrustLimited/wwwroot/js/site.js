97ed41c571aa160d4d4a704aae09626a3c388d68
Olasunkanmi Ariyibi <xadimore@yahoo.com>
2020-04-23 07:25:28 +0100

    Add project files.

---
+++ MoltenTrustLimited/wwwroot/js/site.js
@@ -0,0 +1,40 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+$(".btn").mouseover(function () {
+    $(this).addClass("shadow-lg"),
+
+        $(".btn").mouseleave(function () {
+            $(this).removeClass("shadow-lg")
+        })
+});
+
+
+
+$(document).ready(function () {
+    $("#myCarousel").on("slide.bs.carousel", function (e) {
+        var $e = $(e.relatedTarget);
+        var idx = $e.index();
+        var itemsPerSlide = 3;
+        var totalItems = $(".carousel-item").length;
+
+        if (idx >= totalItems - (itemsPerSlide - 1)) {
+            var it = itemsPerSlide - (totalItems - idx);
+            for (var i = 0; i < it; i++) {
+                // append slides to end
+                if (e.direction == "left") {
+                    $(".carousel-item")
+                        .eq(i)
+                        .appendTo(".carousel-inner");
+                } else {
+                    $(".carousel-item")
+                        .eq(0)
+                        .appendTo($(this).find(".carousel-inner"));
+                }
+            }
+        }
+    });
+});
